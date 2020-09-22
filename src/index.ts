83d7eeb5a81fa792cb9ec33b8a44f105183c178f
nokazn <nokazn@gmail.com>
2020-09-22 21:07:28 +0900

    add basic auth

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import http from 'http';
+// @ts-ignore
+import * as auth from 'http-auth';
 import { router } from './lib/router';
 
+const basic = auth.basic({
+  realm: 'Enter username and passpord.',
+  file: './src/users.htpasswd',
+});
+
+console.log(basic);
 const server = http
-  .createServer((req, res) => {
-    router(req, res);
-  })
-  .on('error', (err) => {
+  .createServer(
+    basic.check((req: http.IncomingMessage, res: http.ServerResponse) => {
+      router(req, res);
+    }),
+  )
+  .on('error', (err: Error) => {
     console.error('Server Error', err);
   })
   .on('clientError', (err: Error) => {
